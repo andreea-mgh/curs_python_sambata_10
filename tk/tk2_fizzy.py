90f8aa59e8975f2fff1e39932f0cde0c73a5203c
Andreea Megherlich <andreea.meghe@gmail.com>
2025-06-28 10:46:21 +0300

    adaugare bubble

--- tk/tk2_fizzy.py
+++ tk/tk2_fizzy.py
@@ -12,6 +12,17 @@ for i in range(25):
     }
     bubbles.append(bubble)
 
+def add_bubble():
+    bubble = {
+        "x": rand(0, 400),
+        "y": 400,
+        "rad": rand(3, 15),
+        "speed": rand(1, 5)
+    }
+    bubbles.append(bubble)
+    root.after(rand(100, 250), add_bubble)
+
+
 def draw_bubbles():
     canvas.delete("all")
     for bubble in bubbles:
@@ -22,7 +33,9 @@ def move_bubble():
     for bubble in bubbles:
         bubble['y'] -= bubble['speed']
         if bubble['y'] < 0:
-            bubble['y'] = 400
+            #bubble['y'] = 400
+            bubbles.remove(bubble)
+
 
 def update():
     move_bubble()
@@ -31,9 +44,10 @@ def update():
 
 root = Tk()
 
-canvas = Canvas(root, width=400, height=400, bg="#331C0B")
+canvas = Canvas(root, width=400, height=400, bg="#FF6A00")
 canvas.pack()
 
 draw_bubbles()
 root.after(20, update)
+root.after(rand(100, 250), add_bubble)
 root.mainloop()
