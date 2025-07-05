2e8b4bfc5ada326cd29a658f313e79ff01171338
Andreea Megherlich <andreea.meghe@gmail.com>
2025-07-05 11:31:16 +0300

    use textures

--- tk/map.py
+++ tk/map.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 
 game_map = ['████████████████',
@@ -13,7 +14,7 @@ game_map = ['████████████████',
 
 game_map = [list(row) for row in game_map]
 
-TILE_SIZE = 16
+TILE_SIZE = 64
 
 MAP_W = len(game_map[0])
 MAP_H = len(game_map)
@@ -25,20 +26,44 @@ canvas = Canvas(root, width =TILE_SIZE*MAP_W,
                       height=TILE_SIZE*MAP_H)
 
 
+wall_img = Image.open('resurse/stone.png').resize((TILE_SIZE, TILE_SIZE))
+root.wall_img = ImageTk.PhotoImage(wall_img)
+
+grass_img = Image.open('resurse/grass.png').resize((TILE_SIZE, TILE_SIZE))
+root.grass_img = ImageTk.PhotoImage(grass_img)
+
+player_img = Image.open('resurse/sprite1.png').resize((TILE_SIZE, TILE_SIZE))
+root.player_img = ImageTk.PhotoImage(player_img)
+
+enemy_img = Image.open('resurse/sprite2.png').resize((TILE_SIZE, TILE_SIZE))
+root.enemy_img = ImageTk.PhotoImage(enemy_img)
+
+coin_img = Image.open('resurse/coin.png').resize((TILE_SIZE, TILE_SIZE))
+root.coin_img = ImageTk.PhotoImage(coin_img)
+
 for y in range(MAP_H):
     for x in range(MAP_W):
-        color = ""
+        # color = ""
         if game_map[y][x] == '█':
-            color = "gray"
+            canvas.create_image(x*TILE_SIZE, y*TILE_SIZE, image=root.wall_img, anchor='nw')
         else:
-            color = "green"
-        
-        x1 = x * TILE_SIZE
-        y1 = y * TILE_SIZE
-        x2 = x1 + TILE_SIZE
-        y2 = y1 + TILE_SIZE
+            canvas.create_image(x*TILE_SIZE, y*TILE_SIZE, image=root.grass_img, anchor='nw')
+            if game_map[y][x] == '$':
+                canvas.create_image(x*TILE_SIZE, y*TILE_SIZE, image=root.coin_img, anchor='nw')
+            elif game_map[y][x] == '!':
+                canvas.create_image(x*TILE_SIZE, y*TILE_SIZE, image=root.enemy_img, anchor='nw')
+            elif game_map[y][x] == 'p':
+                canvas.create_image(x*TILE_SIZE, y*TILE_SIZE, image=root.player_img, anchor='nw')
+            
 
-        canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline='')
+
+
+        # x1 = x * TILE_SIZE
+        # y1 = y * TILE_SIZE
+        # x2 = x1 + TILE_SIZE
+        # y2 = y1 + TILE_SIZE
+
+        # canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline='')
 
 canvas.pack()
 root.mainloop()
